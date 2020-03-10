a327fa9cd10c8572e1b2ce3c05f44a08c6953acb
Jack Steam <teamsteamdev@gmail.com>
2020-03-10 15:26:50 -0500

    Refactor files

---
+++ src/paths.ts
@@ -0,0 +1,4 @@
+import { join } from 'path';
+
+export const PLOP = join(process.cwd(), '.plop')
+export const ELEMENTS =  join(process.cwd(), 'ELEMENTS.md')
--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,20 @@
-import { PlopGenerator } from 'plop'
+import { ActionType, NodePlopAPI, PlopGenerator } from 'plop'
 import { MD } from './parser'
 
 /**
  * Data provided by plop-md
  */
 export interface PlopMdData {
+  /** Full ELEMENTS.md as MD instance */
+  md: MD
   /** Name of file, or H3 */
   filename: string
   /** Section as MD instance */
-  md: MD
+  section: MD
   /** Does this project use React? */
   sections: string[]
 }
 
-export interface PlopMdGenerator extends PlopGenerator {
-  actions: any[]
-}
+export type PlopMdGenerator = (plop: NodePlopAPI) => PlopGenerator
+export type FullPlopGenerator = PlopGenerator & { name: string }
+export type PlopMdAction = ActionType<PlopMdData>
